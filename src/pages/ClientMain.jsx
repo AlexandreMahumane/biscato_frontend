6854a6e888fab7ddf80a99343f39252c9dbd0e04
GeorginaCB <georginadavidcumbe@gmail.com>
2024-08-08 13:59:34 +0200

    client page

--- src/pages/ClientMain.jsx
+++ src/pages/ClientMain.jsx
@@ -1,15 +1,10 @@
-import React, { useState, useRef } from 'react';
-import Modal from 'react-modal';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
-import { XCircle } from "@phosphor-icons/react"; 
-
-Modal.setAppElement('#root');
+import { XCircle } from "@phosphor-icons/react";
 
 const ClientMain = () => {
-  const [feedbackModalOpen, setFeedbackModalOpen] = useState(false);
-  const [historyModalOpen, setHistoryModalOpen] = useState(false);
   const [query, setQuery] = useState('');
   const [category, setCategory] = useState('');
   const [description, setDescription] = useState('');
@@ -17,107 +12,29 @@ const ClientMain = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
   const [noResults, setNoResults] = useState(false);
-  const [showAlert, setShowAlert] = useState(false); // Novo estado para o alerta
-  const abortControllerRef = useRef(null);
+  const [showAlert, setShowAlert] = useState(false);
+  const [expandedSection, setExpandedSection] = useState(null);
 
   const successMessage = "Your registration was successful!";
 
-  const FeedbackModal = () => {
-    const [feedback, setFeedback] = useState('');
-
-    const submitFeedback = () => {
-      console.log('Feedback enviado:', feedback);
-      setFeedbackModalOpen(false);
-      setShowAlert(true); // Exibe o alerta após enviar o feedback
-    };
-
-    const handleClose = () => {
-      setFeedbackModalOpen(false);
-    };
-
-    return (
-      <Modal
-        isOpen={feedbackModalOpen}
-        onRequestClose={handleClose}
-        contentLabel="Feedback Modal"
-        className="modal-overlay"
-        overlayClassName="modal-overlay"
-      >
-        <div className="modal-content">
-          <button
-            onClick={handleClose}
-            className="btn-close absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-          >
-            <XCircle size={24} />
-          </button>
-          <div className="bg-green-100 text-green-800 p-6 rounded-lg shadow-md max-w-md mx-auto">
-            <label htmlFor="feedback" className="block text-lg font-semibold text-gray-800 mb-2">
-              Feedback:
-            </label>
-            <textarea
-              id="feedback"
-              value={feedback}
-              onChange={(e) => setFeedback(e.target.value)}
-              placeholder="Enter your feedback..."
-              className="border border-gray-300 rounded-md p-2 w-full h-20 resize-none mb-4"
-            ></textarea>
-            <button
-              onClick={submitFeedback}
-              className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-            >
-              Submit Feedback
-            </button>
-          </div>
-        </div>
-      </Modal>
-    );
-  };
-
-  const HistoryModal = () => {
-    const handleClose = () => {
-      setHistoryModalOpen(false);
-    };
-
-    return (
-      <Modal
-        isOpen={historyModalOpen}
-        onRequestClose={handleClose}
-        contentLabel="History Modal"
-        className="modal-overlay"
-        overlayClassName="modal-overlay"
-      >
-        <div className="modal-content">
-          <button
-            onClick={handleClose}
-            className="btn-close absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-          >
-            <XCircle size={24} />
-          </button>
-          <div className="bg-green-100 text-green-800 p-6 rounded-lg shadow-md max-w-md mx-auto">
-            <h3 className="text-lg font-bold mb-4">History of services:</h3>
-            <ul className="list-disc pl-5">
-              <li>Service 1: Date: 2024-08-01, Time: 10:00 AM, Service: Consultation</li>
-              <li>Service 2: Date: 2024-08-03, Time: 02:00 PM, Service: Follow-up</li>
-            </ul>
-          </div>
-        </div>
-      </Modal>
-    );
+  const toggleExpandedSection = (section) => {
+    if (expandedSection === section) {
+      setExpandedSection(null); 
+    } else {
+      setExpandedSection(section); 
+    }
   };
 
   const handleSearch = async () => {
-    // Lógica para realizar a pesquisa de freelancers
-
-    // Exemplo simples para demonstração
     setLoading(true);
     setError('');
     setNoResults(false);
 
     try {
-      // Simulando uma resposta com resultados
       const mockResults = [{ name: 'Freelancer 1' }, { name: 'Freelancer 2' }];
       setResults(mockResults);
-      setShowAlert(true); 
+      setShowAlert(true);
+      setExpandedSection('results'); 
     } catch (error) {
       setError('Failed to fetch results. Please try again.');
     } finally {
@@ -132,17 +49,17 @@ const ClientMain = () => {
     <>
       <Header showAlert={showAlert} setShowAlert={setShowAlert} />
       <div className="container mx-auto p-4 md:flex md:justify-between">
-        <div className="bg-green-100 text-green-800 p-6 rounded-lg shadow-md max-w-md mx-auto mt-6 md:mr-4 transition-transform duration-300 hover:scale-105">
+        <div className={`bg-green-100 text-green-800 p-6 rounded-lg shadow-md max-w-md mx-auto mt-6 md:mr-4 transition-transform duration-300 hover:scale-105 ${expandedSection === null ? '' : 'hidden'}`}>
           <h1 className="text-3xl font-semibold text-gray-800 mb-4">Welcome to Your Client Dashboard</h1>
           <div className="flex justify-around">
             <button
-              onClick={() => setFeedbackModalOpen(true)}
+              onClick={() => toggleExpandedSection('feedback')}
               className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
             >
               Send Feedback
             </button>
             <button
-              onClick={() => setHistoryModalOpen(true)}
+              onClick={() => toggleExpandedSection('history')}
               className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
             >
               View History
@@ -150,10 +67,50 @@ const ClientMain = () => {
           </div>
         </div>
 
-        <FeedbackModal />
-        <HistoryModal />
+        
+        {expandedSection === 'feedback' && (
+          <div className="bg-green-100 text-green-800 p-6 rounded-lg shadow-md max-w-md mx-auto mt-6 md:mr-4 transition-transform duration-300 hover:scale-105">
+            <div className="flex justify-end">
+              <button className="text-gray-600 hover:text-gray-800 cursor-pointer" onClick={() => setExpandedSection(null)}>
+                <XCircle size={24} />
+              </button>
+            </div>
+            <h1 className="text-3xl font-semibold text-gray-800 mb-4">Feedback Section</h1>
+            <label htmlFor="feedback" className="block text-lg font-semibold text-gray-800 mb-2">
+              Feedback:
+            </label>
+            <textarea
+              id="feedback"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Enter your feedback..."
+              className="border border-gray-300 rounded-md p-2 w-full h-20 resize-none mb-4"
+            ></textarea>
+            <button
+              onClick={() => {  }}
+              className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+            >
+              Submit Feedback
+            </button>
+          </div>
+        )}
 
-        <div className="bg-green-100 text-green-800 p-6 rounded-lg shadow-md max-w-md mx-auto mt-6 md:mr-4 transition-transform duration-300 hover:scale-105">
+        {expandedSection === 'history' && (
+          <div className="bg-green-100 text-green-800 p-6 rounded-lg shadow-md max-w-md mx-auto mt-6 md:mr-4 transition-transform duration-300 hover:scale-105">
+            <div className="flex justify-end">
+              <button className="text-gray-600 hover:text-gray-800 cursor-pointer" onClick={() => setExpandedSection(null)}>
+                <XCircle size={24} />
+              </button>
+            </div>
+            <h1 className="text-3xl font-semibold text-gray-800 mb-4">History Section</h1>
+            <ul className="list-disc pl-5">
+              <li>Service 1: Date: 2024-08-01, Time: 10:00 AM, Service: Consultation</li>
+              <li>Service 2: Date: 2024-08-03, Time: 02:00 PM, Service: Follow-up</li>
+            </ul>
+          </div>
+        )}
+
+        <div className={`bg-green-100 text-green-800 p-6 rounded-lg shadow-md max-w-md mx-auto mt-6 md:mr-4 transition-transform duration-300 hover:scale-105 ${expandedSection === null ? '' : 'hidden'}`}>
           <h2 className="text-lg font-bold mb-4 text-center">Freelancer Information</h2>
           <div className="mb-4">
             <select
@@ -198,10 +155,10 @@ const ClientMain = () => {
       </div>
 
       {loading && <p className="text-center text-gray-500">Loading...</p>}
-      {error && <p className="text-center text-red-500">{error}</p>}
+      {error && <p className="text-center text-red-800">{error}</p>}
       {noResults && <p className="text-center text-gray-500">No results found.</p>}
-      {!loading && !noResults && results.length > 0 && (
-        <div className="container mx-auto p-4">
+      {results.length > 0 && (
+        <div className={`container mx-auto p-4 ${expandedSection === 'results' ? '' : 'hidden'}`}>
           <ul className="list-disc pl-5">
             {results.map((result, index) => (
               <li key={index} className="mb-2">{result.name}</li>
@@ -210,7 +167,7 @@ const ClientMain = () => {
         </div>
       )}
 
-      <Footer/>
+      <Footer />
     </>
   );
 };
